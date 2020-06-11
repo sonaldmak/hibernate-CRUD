298502e4e7d6026926a05ddb125054c7f2a67a27
sonaldmak <sonalpote96@gmail.com>
2020-06-11 13:50:07 -0400

    Hibernate_CRUDoperation

---
+++ Hibernate_Curd_Example/src/main/java/hibernate/model/Student.java
@@ -0,0 +1,63 @@
+package hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="Student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstname;
+	
+	@Column(name = "last_name")
+	private String lastname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Student(long id, String firstname, String lastname, String email) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
